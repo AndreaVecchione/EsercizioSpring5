ace2cc4c2d97300d3520f8048d0610a00f1ed0a6
Andrea Vecchione <andrea.vecchione02@gmail.com>
2024-09-23 02:10:01 +0200

    EsercizioSpring5

---
+++ src/main/java/com/example/EsercizioSpring5/EsercizioSpring5Application.java
@@ -0,0 +1,13 @@
+package com.example.EsercizioSpring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioSpring5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EsercizioSpring5/EsercizioSpring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsercizioSpring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioSpring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
